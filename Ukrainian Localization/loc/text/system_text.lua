1ec6f6e215ab14e084d2ea39b9fa73f235bb9227
FutureSonic <40641530+FutureSonic-TH@users.noreply.github.com>
2026-03-26 22:58:24 +0200

    Виправлення відображення локалізації на SuperBLT
    Було виправлено відображення української локалізації на SuperBLT для PayDay 2. Бо не було відповідних hook-ів, які могли підвантажувати файли локалізації.

--- Ukrainian Localization/loc/text/system_text.lua
+++ Ukrainian Localization/loc/text/system_text.lua
@@ -1,4 +1,6 @@
-return {
+Hooks:Add("LocalizationManagerPostInit", "UK_system_text", function(loc) 
+	LocalizationManager:add_localized_strings({
+
 	wp_ap_escape = "Втеча!",
 	wp_bank_escape = "Втеча!",
 	wp_bridge_escape = "Втеча!",
@@ -24,4 +26,6 @@ return {
 	wp_xsub_waterhose = "Потягніть за шланг",
 	wp_xsub_watertap = "Відкрий кран",
 	wp_xsub_watervalve = "Відкрийте вентиль",
-	}
+	
+		})
+end)
