e787b5ec1ed3b75a923472f3859ffcb0355a2caa
PAVLO PARAPANDO KATUNIN <69468857+Parapando@users.noreply.github.com>
2026-09-25 03:29:59 +0200

    Add files via upload

---
+++ Ukrainian Localization/loc/savefile.lua
@@ -0,0 +1,11 @@
+return {
+	savefile_autosave = "$TEXT_WITH_DATE; - АВТОЗБЕРЕЖЕННЯ",
+	savefile_empty = "[Новий Файл Збереження]",
+	savefile_loading = "Завантаження...",
+	savefile_progress = "Ігрова прогресія: Потрібна Версія $VERSION; або пізніша.",
+	savefile_progress_description = "Статистика гравця та ігрова прогресія.",
+	savefile_removing = "Видалення...",
+	savefile_saving = "Збереження...",
+	savefile_setting = "Ігрові налаштування: Потрібна Версія $VERSION; або пізніша.",
+	savefile_setting_description = "Специфічні ігрові налаштування користувача.",
+	}
